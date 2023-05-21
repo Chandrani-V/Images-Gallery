ba761df1dfb07fdbd7bbcda735a1b4cead8a8f7c
Chandrani V <v.chandrani.123@gmail.com>
2023-05-21 11:53:22 +0530

    header and controlled search

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Search from './components/Search';
+import { useState } from 'react'
+// {} is not needed for header bcs it is exported as default
+
 
 function App() {// can also be written as const App ]()=>
+    const [word, setWord] = useState('');
+    const handleSearchSubmit = (e) => {
+        e.preventDefault()
+        console.log(word)
+    }
     return (//returns JSX -div
       // this is for creating h1, div elemenets
-    <div>
-          <h1> Images Gallery</h1> 
+        <div>
+            <Header title='Images Gallery' />
+            <Search word={word } setWord={setWord} handleSubmit={ handleSearchSubmit } />
     </div>
   );
 }
