ef8c1aec54d1d7a4f4463d392ec6e230ee0b4451
Chandrani V <v.chandrani.123@gmail.com>
2023-05-30 10:00:19 +0530

    python venv

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -7,14 +7,15 @@ const Welcome = () =>
      (
         <div class="jumbotron">
         <h1> Hello, This is Images Gallery</h1>
-        <p>
-            An application for retrieving photos using Unsplash API.
+        <p>An application for retrieving photos using Unsplash API.
         </p>
         <p>Please enter your search term in input field to search for photos </p>
         <p>
-           <a class="btn btn-primary btn-lg" href="https://unsplash.com" role="button">Learn more</a>
+        <a class="btn btn-primary btn-lg" href="https://unsplash.com" role="button">Learn more</a>
         </p>
-        </div>
+    </div>
+
+
 
 );
 export default Welcome;
